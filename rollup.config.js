4e728d0fa24b1fc09fcf6412d4116adfbcbfd0d8
Ben Gimbel <bgimbel@thecitybase.com>
2019-09-18 16:17:54 -0500

    added rollupJS

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,31 @@
-import resolve from "rollup-plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
-import pkg from "./package.json";
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import pkg from './package.json';
 
 export default [
-  // browser-friendly UMD build
-  {
-    input: "src/index.js",
-    output: {
-      name: "formatted-input",
-      file: pkg.browser,
-      format: "umd"
+    {
+        input: 'src/index.js',
+        output: {
+            name: 'index',
+            file: pkg.main,
+            format: 'umd'
+        },
+        plugins: [
+            resolve(),
+            babel({ 
+                exclude: 'node_modules/**',
+                presets: ['@babel/env', '@babel/preset-react']
+            }),   
+            commonjs(),
+            // rollupNodeResolve(), rollupBabel(), rollupCommonJS()
+        ],
+        external: [
+            'react',
+            'prop-types',
+        ],
+        globals: {
+            react: "React"
+        }
     },
-    plugins: [resolve(), commonjs()]
-  },
-  {
-    input: "src/index.js",
-    output: [
-      { file: pkg.main, format: "cjs" },
-      { file: pkg.module, format: "es" }
-    ],
-    plugins: [resolve(), commonjs()]
-  }
 ];
