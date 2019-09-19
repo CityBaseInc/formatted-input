1c990afcf006ff4da9561b21e71ed36adc100195
Andy McConnell <amcconnell@thecitybase.com>
2019-09-19 14:45:23 -0500

    updating examples

--- examples/ExampleForm/index.js
+++ examples/ExampleForm/index.js
@@ -1,40 +1,9 @@
 import React from "../../node_modules/react";
 import ReactDOM from "../../node_modules/react-dom";
-import FormattedInput from "../../src/FormattedInput";
-import { createFormat } from "../../src/FormattedInput";
 import ExampleForm from "./exampleForm";
-const phoneFormats = [
-  "",
-  "+_",
-  "+_ _",
-  "+_ __",
-  "+_ (___) ",
-  "+_ (___) _",
-  "+_ (___) __",
-  "+_ (___) ___ - ",
-  "+_ (___) ___ - _",
-  "+_ (___) ___ - __",
-  "+_ (___) ___ - ___",
-  "+_ (___) ___ - ____",
-  "+__ (___) ___ - ____",
-  "+___ (___) ___ - ____"
-];
 
-const dateFormats = [
-  "",
-  "_",
-  "__/",
-  "__/_",
-  "__/__/",
-  "__/__/_",
-  "__/__/__",
-  "__/__/___",
-  "__/__/____"
-];
-const rootEl = document.getElementById("example-form");
+const exampleForm = document.getElementById("example-form");
 ReactDOM.render(
-<div>
-  <FormattedInput value={""} formatter={createFormat(phoneFormats, "_")} />
-  <FormattedInput value={""} formatter={createFormat(dateFormats, "_")} />
-</div>
-, rootEl);
+  <ExampleForm />,
+  exampleForm
+);
