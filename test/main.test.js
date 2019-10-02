6baacab6d4e7d3745f520396a01cfdccd02212c9
Ben Gimbel <bgimbel@thecitybase.com>
2019-10-02 12:12:02 -0500

    added first test

--- test/main.test.js
+++ test/main.test.js
@@ -1,8 +1,16 @@
-import test from "ava";
-
+import test, { beforeEach } from "ava";
+import {shallow} from "enzyme";
 import { FormattedInput, createFormat } from '../src/FormattedInput';
 
+describe("<FormattedInput />"), () => {
+  let wrapper;
+  let props;
 
-test("", t => {
+  beforeEach(() => {
+     wrapper = shallow(<FormattedInput {...props} />)
+  });
 
-});
+  it("renders the FormattedInput component"), () => {
+    wrapper.find("input").length.toBe(1);
+  }
+}
