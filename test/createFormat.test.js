82133e8424ad1548f85fd78097ec3b17bc4de232
Andy McConnell <amcconnell@thecitybase.com>
2019-12-03 11:41:11 -0600

    rollback test update

--- test/createFormat.test.js
+++ test/createFormat.test.js
@@ -5,13 +5,17 @@ import { phoneFormats, dateFormats } from "./utils";
 test("creates a formatter correctly", t => {
   const phoneFormatsPlaceholder = "_";
   const formatter = createFormat(phoneFormats, phoneFormatsPlaceholder);
+  const expectedUniqueDelimiters = ["+", " ", "(", ")", "-"];
   t.deepEqual(formatter.formatChar, phoneFormatsPlaceholder);
   t.deepEqual(formatter.formats, phoneFormats);
+  t.deepEqual(formatter.uniqueDelimiters, expectedUniqueDelimiters);
 });
 
 test("creates a formatter correctly with a different placeholder", t => {
   const dateFormatsPlaceholder = "x";
   const formatter = createFormat(dateFormats, dateFormatsPlaceholder);
+  const expectedUniqueDelimiters = ["/"];
   t.deepEqual(formatter.formatChar, dateFormatsPlaceholder);
   t.deepEqual(formatter.formats, dateFormats);
+  t.deepEqual(formatter.uniqueDelimiters, expectedUniqueDelimiters);
 });
