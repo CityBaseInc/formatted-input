718e007a9498e514c5e710e8c49f3735c65aa349
Andy McConnell <amcconnell@thecitybase.com>
2020-06-22 15:14:05 -0500

    adding resolutions to package.jsons, updating rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,57 +1,20 @@
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import babel from "rollup-plugin-babel";
-import pkg from "./package.json";
-
-export default [
-  {
-    input: "src/index.js",
-    output: {
-      name: "index",
-      file: pkg.main,
-      format: "umd",
-      globals: {
-        react: "React"
-      }
-    },
-    plugins: [
-      resolve(),
-      babel({
-        exclude: "node_modules/**",
-        presets: ["@babel/env", "@babel/preset-react"]
-      }),
-      commonjs()
-    ],
-    external: ["react", "prop-types"]
-  }
-];
-
-import resolve from "rollup-plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
-import babel from "rollup-plugin-babel";
 import visualizer from "rollup-plugin-visualizer";
 import pkg from "./package.json";
 
-import * as formattedInput from "formatted-input";
-
 const globals = {
-  react: "React",
-  "react-dom": "ReactDOM"
+  react: "React"
 };
 
 const plugins = [
-  resolve({ preferBuiltins: false }),
+  resolve(),
   babel({
     exclude: "node_modules/**",
     presets: ["@babel/env", "@babel/preset-react"]
   }),
-  json(),
-  commonjs({
-    include: "node_modules/**",
-    namedExports: {
-      "formatted-input": Object.keys(formattedInput)
-    }
-  }),
+  commonjs(),
   visualizer()
 ];
 
